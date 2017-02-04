a50ec90e6f27bd7ad719460cc8fcb2f2c0b8d568
Dukeman <gduke@bullhorn.com>
2017-02-03 23:59:38 -0500

    adding config options

--- fogcast/cli.py
+++ fogcast/cli.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
-
+import sys
 import click
+from loadconfig import Config
+
+sys.path.append('/fogcast/fogcast')
+import fogcast
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.command()
-def main(args=None):
-    """Console script for fogcast"""
-    click.echo("Replace this message by putting your code into "
-               "fogcast.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.command(context_settings=CONTEXT_SETTINGS)
+def main():
+    """Return a chance of fog for any hour in the subsequent 48 hours where the difference
+     between dewpoint and temperature is greater than 5 degrees"""
+    conf = '!include config.yml'
+    c = Config(conf)
+
+    print(c.forecast_key)
+    res = fogcast.fetch_forecast_by_hour(c.API_KEYS.forecast_key, c.VARS.lat, c.VARS.lng)
+    fogcast.calc_fog_from_dewpoint_diff(res)
 
 
 if __name__ == "__main__":
--- fogcast/fogcast.py
+++ fogcast/fogcast.py
@@ -1,1 +1,36 @@
-# -*- coding: utf-8 -*-
+# fetching weather
+import sys
+import click
+import forecastio
+
+
+def fetch_forecast_by_day(forecast_key, lat, lng):
+    forecast = forecastio.load_forecast(c.forecast_key, c.lat, c.lng)
+    return forecast.daily()
+
+
+def fetch_forecast_by_hour(forecast_key, lat, lng):
+    forecast = forecastio.load_forecast(forecast_key, lat, lng)
+    return forecast.hourly()
+
+
+def calc_fog_from_dewpoint_diff(response_by_hour):
+    fogTimes = []
+    debug = []
+
+    for hourlyData in response_by_hour.data:
+        deg = hourlyData.temperature
+        dew = hourlyData.dewPoint
+        diff = abs(dew - deg)
+        debug.append('{:%a: %I %P}'.format(hourlyData.time) + " | dewpoint = " + str(int(dew)) + " | deg = " + str(
+            int(deg)) + " | diff = " + str(int(diff)))
+        if deg <= 66 and int(diff) <= 5:
+            fogTimes.append('Chance of fog on ' + '{:%a at %I %P}'.format(hourlyData.time))
+
+    if len(sys.argv) > 1 and "debug" in sys.argv[1:]:
+        click.echo('\n').join(debug)
+
+    if not fogTimes:
+        click.echo('There is no fog forecasted in the next 48 hours')
+    else:
+        click.echo('\n').join(fogTimes)
